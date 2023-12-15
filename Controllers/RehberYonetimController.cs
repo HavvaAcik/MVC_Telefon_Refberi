cb4e7b71359322c91df0de33b09a3528c5a2d534
Havva Açık <acikhavva184@gmail.com>
2023-12-15 18:28:43 +0300

    kisi ekleme sayfası eklendi

--- Controllers/RehberYonetimController.cs
+++ Controllers/RehberYonetimController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MVCRehberUyg.Data;
+using MVCRehberUyg.Models;
 
 namespace MVCRehberUyg.Controllers
 {
@@ -27,6 +28,44 @@ namespace MVCRehberUyg.Controllers
         {
             return View(model:_context.Kisilers);
         }
+
+        [HttpGet]
+        public IActionResult Ekle() // ekleme formuna yönlendirecek 
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Ekle(Kisiler yeniKisi) // formdan gelen kişiyi ekleyecek
+        {
+            _context.Kisilers.Add(yeniKisi);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Detaylar(int id)
+        {
+            // hangi id hangi personelin?
+            var kisiler = _context.Kisilers.SingleOrDefault(k => k.KisiNo ==id);
+            return View(model:kisiler);
+        }
+        [HttpGet]
+        public IActionResult Duzenle(int id) // formu açıyor 
+        {
+            var kisiler = _context.Kisilers.SingleOrDefault(k => k.KisiNo ==id);
+            return View(model:kisiler);
+        }
+        [HttpPost]
+        public IActionResult Duzenle(Kisiler duzenlenmisKisiler) // formdan gelen veriyi açıyor.
+        {
+            var kisiler = _context.Kisilers.SingleOrDefault(k => k.KisiNo == duzenlenmisKisiler.KisiNo);
+            kisiler.Ad= duzenlenmisKisiler.Ad;
+            kisiler.Soyad=duzenlenmisKisiler.Soyad;
+            kisiler.TelefonNo=duzenlenmisKisiler.TelefonNo;
+            return RedirectToAction("Index");
+        }
+        
+        
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Data/SanalVeritabani.cs
+++ Data/SanalVeritabani.cs
@@ -10,14 +10,14 @@ namespace MVCRehberUyg.Data
     {
         public List<Kisiler> Kisilers = new List<Kisiler>()
         {
-          new Kisiler {KisiNo = 1001, Ad = "Ahmet", Soyad = "ÇOKÇALIŞIR",TelefonNo=05550000000, Resim = "ahmet.jpg"},
-          new Kisiler {KisiNo = 1, Ad = "Hasan", Soyad = "YORULMAZ",TelefonNo=05555555555,Resim = "hasan.jpg"},
-          new Kisiler {KisiNo = 2, Ad = "Ayşe", Soyad = "İŞYAPAR",TelefonNo=05554444444,Resim = "ayse.jpg"},
-          new Kisiler {KisiNo = 3, Ad = "Hatice", Soyad = "İŞBİLİR",TelefonNo=05553333333, Resim = "hatice.jpg"},
-          new Kisiler {KisiNo = 4, Ad = "Zeynep", Soyad = "HEPÇALIŞIR",TelefonNo=05556666666, Resim = "zeynep.jpg"},
-          new Kisiler {KisiNo = 5, Ad = "Mehmet", Soyad = "HİÇDURMAZ",TelefonNo=05557777777, Resim = "mehmet.jpg"},
-          new Kisiler {KisiNo = 6, Ad = "Hüseyin", Soyad = "KULAKASMAZ",TelefonNo=05558888888, Resim = "huseyin.jpg"},
-          new Kisiler {KisiNo = 1008, Ad = "Fatma", Soyad = "DURAKBİLMEZ",TelefonNo=05559999999, Resim = "fatma.jpg"},
+          new Kisiler {KisiNo = 1, Ad = "Ahmet", Soyad = "ÇOKÇALIŞIR",TelefonNo=05550000000, Resim = "ahmet.jpg"},
+          new Kisiler {KisiNo = 2, Ad = "Hasan", Soyad = "YORULMAZ",TelefonNo=05555555555,Resim = "hasan.jpg"},
+          new Kisiler {KisiNo = 3, Ad = "Ayşe", Soyad = "İŞYAPAR",TelefonNo=05554444444,Resim = "ayse.jpg"},
+          new Kisiler {KisiNo = 4, Ad = "Hatice", Soyad = "İŞBİLİR",TelefonNo=05553333333, Resim = "hatice.jpg"},
+          new Kisiler {KisiNo = 5, Ad = "Zeynep", Soyad = "HEPÇALIŞIR",TelefonNo=05556666666, Resim = "zeynep.jpg"},
+          new Kisiler {KisiNo = 6, Ad = "Mehmet", Soyad = "HİÇDURMAZ",TelefonNo=05557777777, Resim = "mehmet.jpg"},
+          new Kisiler {KisiNo = 7, Ad = "Hüseyin", Soyad = "KULAKASMAZ",TelefonNo=05558888888, Resim = "huseyin.jpg"},
+          new Kisiler {KisiNo = 8, Ad = "Fatma", Soyad = "DURAKBİLMEZ",TelefonNo=05559999999, Resim = "fatma.jpg"},
         };
     }
 }
